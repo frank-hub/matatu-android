16e1a8e6f138c0dfdc7f33f40673c7023b5a1177
frank-hub <franklinekevo@gmail.com>
2019-05-19 21:49:00 +0300

    initial

---
+++ app/src/main/java/com/example/frankline/myclinic/HealthTips.java
@@ -0,0 +1,13 @@
+package com.example.frankline.myclinic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HealthTips extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health_tips);
+    }
+}
