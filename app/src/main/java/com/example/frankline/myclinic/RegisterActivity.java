b87417785cd035b74ecc1a3a3c83d9a0934ec29c
frank-hub <franklinekevo@gmail.com>
2019-05-20 03:59:12 +0300

    register backend

--- app/src/main/java/com/example/frankline/myclinic/RegisterActivity.java
+++ app/src/main/java/com/example/frankline/myclinic/RegisterActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -81,6 +82,8 @@ public class RegisterActivity extends AppCompatActivity {
                             showToken(response.body());
                         }else{
                             showMessage("User could not be registered...try again.");
+                            showMessage(""+response.toString());
+                            Log.e("WB",response.raw().body().toString());
                         }
                     }
 
